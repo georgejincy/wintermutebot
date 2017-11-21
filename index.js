6ef9f606387d2914a02e6024e35f0674770df276
Jincy George <jincy.george@bhemail.com>
2017-11-21 09:40:04 -0500

    :pencil: Start RTM and give bot something to listen for

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const path = require('path')
 const botkit = require('botkit')
-const token = require(path.join(__dirname, 'config.json'))['SLACK_API_TOKEN']
+const token = 'SLACK_API_TOKEN'
+const slackAPIToken = require(path.join(__dirname, 'config.json'))[token]
+
+var controller = botkit.slackbot({ debug: true })
+
+controller
+  .spawn({ token: slackAPIToken })
+  .startRTM(function (err, bot, payload) {
+    if (err) {
+      throw new Error('Could not connect to Slack')
+    }
+
+    // close the RTM for the sake of it in 5 seconds
+    setTimeout(function () {
+      bot.closeRTM()
+    }, 5000)
+  })
+
+// give the bot something to listen for.
+controller.hears(
+  'hello',
+  ['direct_message', 'direct_mention', 'mention'],
+  function (bot, message) {
+    bot.reply(message, 'Hello')
+  }
+)
