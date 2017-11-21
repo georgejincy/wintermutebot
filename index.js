c4b70c0c55dd3164d627c91365d58b71ff785da2
Jincy George <jincy.george@bhemail.com>
2017-11-21 11:46:42 -0500

    :pencil: get username

--- index.js
+++ index.js
@@ -5,24 +5,21 @@ const slackAPIToken = require(path.join(__dirname, 'config.json'))[token]
 
 var controller = botkit.slackbot({ debug: true })
 
-controller
-  .spawn({ token: slackAPIToken })
-  .startRTM(function (err, bot, payload) {
-    if (err) {
-      throw new Error('Could not connect to Slack')
-    }
-
-    // close the RTM for the sake of it in 5 seconds
-    setTimeout(function () {
-      bot.closeRTM()
-    }, 5000)
-  })
+controller.spawn({ token: slackAPIToken }).startRTM()
 
 // give the bot something to listen for.
 controller.hears(
-  'hello',
-  ['direct_message', 'direct_mention', 'mention'],
+  ['hello', 'hi'],
+  'direct_message,direct_mention,mention',
   function (bot, message) {
-    bot.reply(message, 'Hello')
+    let currentUser
+    bot.api.users.info({ user: message.user }, function (err, response) {
+      if (err) {
+        bot.reply('Hello')
+      } else {
+        currentUser = response['user']
+        bot.reply(message, 'Hello @' + currentUser['name'])
+      }
+    })
   }
 )
