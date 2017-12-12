143b5ff4c088c00097d14ec0d3e94ac0e9fc31ed
Jincy George <jincy.george@bhemail.com>
2017-12-12 16:24:24 -0500

    :sparkles: Healthcheck for ringleader and knysna RIN41, RIN42

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const botkit = require('botkit')
+const request = require('request-promise')
 const token = 'SLACK_API_TOKEN'
 const slackAPIToken = require(path.join(__dirname, 'config.json'))[token]
 
@@ -7,7 +8,7 @@ var controller = botkit.slackbot({ debug: true })
 
 controller.spawn({ token: slackAPIToken }).startRTM()
 
-// give the bot something to listen for.
+// Bot listens for hello / hi.
 controller.hears(
   ['hello', 'hi'],
   'direct_message,direct_mention,mention',
@@ -21,5 +22,61 @@ controller.hears(
         bot.reply(message, 'Hello @' + currentUser['name'])
       }
     })
+  }
+)
+
+// Bot listens for ringleader.
+controller.hears(
+  ['ringleader', 'ringleader health'],
+  'direct_message,direct_mention,mention',
+  function (bot, message) {
+    request({
+      method: 'GET',
+      uri: 'http://ringleader.int.bhwebapps.com/healthcheck',
+      json: true
+    })
+      .then(function (response) {
+        console.log('Blabla')
+        console.log(JSON.stringify(response))
+        if (response.timestamp) {
+          bot.reply(
+            message,
+            'Ringleader is healthy . timestamp:' + response.timestamp
+          )
+        } else {
+          bot.reply(message, 'Ringleader is down :(')
+        }
+      })
+      .catch(function (err) {
+        bot.reply(message, 'Ringleader is down :(' + err)
+      })
+  }
+)
+
+// Bot listens for knysna.
+controller.hears(
+  ['knysna', 'knysna health'],
+  'direct_message,direct_mention,mention',
+  function (bot, message) {
+    request({
+      method: 'GET',
+      uri: 'http://172.20.20.20:8080/v1/healthcheck',
+      json: true
+    })
+      .then(function (response) {
+        console.log('Blabla')
+        console.log(JSON.stringify(response))
+        if (response.timestamp) {
+          bot.reply(
+            message,
+            'Knysna is alive and well . timestamp:' + response.timestamp
+          )
+        } else {
+          bot.reply(message, 'Knysna is unwell :(')
+        }
+      })
+      .catch(function (err) {
+        bot.reply(message, 'Knysna is unwell :(' + err)
+      })
   }
 )
